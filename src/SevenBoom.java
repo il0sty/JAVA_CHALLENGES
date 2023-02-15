c7b8773a340270361dbae9abab64f0972ccdd100
Antonio Neto <neto250301@gmail.com>
2023-02-15 10:33:43 -0300

    Comments added

--- src/SevenBoom.java
+++ src/SevenBoom.java
@@ -35,3 +35,6 @@ public class SevenBoom {
         return check;
     }
 }
+
+//Create a function that takes an array of numbers and return "Boom!" if the digit 7 appears in the array.
+//Otherwise, return "there is no 7 in the array".
